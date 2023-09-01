6846890c4caba97f2ca5111293246fca0a0f1599
sajib37 <sajibhasan9811@gmail.com>
2023-09-01 23:51:24 +0600

    blog page completed

--- js/javascript.js
+++ js/javascript.js
@@ -23,17 +23,24 @@ const catagoryButton=(id,name)=>{
 
 // data show by clicking button
 const cardContainer = document.getElementById('card-container');
+const noContent=document.getElementById('no-content')
 const showData =async (id) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
     const data = await response.json();
     cardContainer.innerHTML = '';
+    if (data.data.length == 0) {
+        noContent.classList.remove('hidden');
+    }
+    else{
+        noContent.classList.add('hidden');
+    }
     data.data.forEach(content => {
         const div = document.createElement('div');
         div.innerHTML = `
         <div class="card card-compact max-w-xs bg-base-100 shadow-xl mx-auto rounded-none">
             <figure class="relative" >
                 <img class="w-full h-[200px] rounded-none" src="${content.thumbnail}" alt="Shoes" />
-                <p class="text-xs text-white  bg-black inline absolute bottom-0 right-0">${timeConverter(content.others?.posted_date)}</p>
+                <p id="timeContainer" class="text-xs text-white  bg-black inline absolute bottom-0 right-0">${timeConverter(content.others?.posted_date)}</p>
             </figure>
             <div class="card-body p-0">
               <div class="flex  gap-4">
@@ -44,26 +51,29 @@ const showData =async (id) => {
               <p class="text-gray-400 text-sm"> ${content.others?.views} views</p>
             </div>
         </div>
-        `
+        `        
         cardContainer.appendChild(div);
     })
 }
-
 // function for time coverter (sec to min and hour)
 const timeConverter = (sec) => {
-    console.log(sec)
     let second = sec;
     let hour = Math.floor(second / 3600);
     let reminderSec = second % 3600;
     let min = Math.floor(reminderSec / 60);
-    if (sec == 0 || sec == null || sec == undefined) {
+    if (sec == 0) {
         return '';
     }
     else {
         return hour + " Hrs " + min + " Mins ago";
     } 
 }
-console.log(timeConverter(3680));
 handleCatagories();
 // for default shows in a screen
-showData(1001)
+showData(1000)
+
+
+// blogg new page Open
+document.getElementById('Blog').addEventListener('click', function () {
+    window.location.href="blog.html"
+})
